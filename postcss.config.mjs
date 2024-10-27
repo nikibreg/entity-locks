88ecee51460f71c619dcddbe4d5dccd6b7520604
nikibreg <nbregv@yahoo.co.uk>
2024-10-28 01:46:45 +0400

    fix tailwind

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {
+      content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+     
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+      ],
+    },
   },
 };
 
